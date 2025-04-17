0842fb42bac39a7dd2c5fbfd3a5030b4fa5461c3
MamaniMarioOrlando <mario_7_89@hotmail.com>
2025-04-17 20:36:07 -0300

    create Entityes and config application.yml

---
+++ src/main/java/com/restaurante_rio/resto/entity/Order.java
@@ -0,0 +1,26 @@
+package com.restaurante_rio.resto.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Long restaurantId;
+    private Double total;
+    private String status;
+    private LocalDateTime createAt;
+}
---
+++ src/main/java/com/restaurante_rio/resto/entity/Restaurant.java
@@ -0,0 +1,23 @@
+package com.restaurante_rio.resto.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "restaurants")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private String phone;
+    private String cuisineType;
+}
